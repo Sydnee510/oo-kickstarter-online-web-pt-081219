be412ae92f58666d7f3f3a69dff2ea22da21e76a
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-19 04:49:05 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,6 +7,7 @@ class Backer
   end
   def back_project(project)
     @backed_projects << project
+     project.add_backer(self) unless project.backers.include?(self)
     # add_backer(backer)
   end 
 end
